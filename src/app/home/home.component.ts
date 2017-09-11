2f141a78895f94c405947e067119983cb3aee3c3
Yatin <yatinweb@gmail.com>
2017-09-11 20:20:47 +0530

    - Added happy request

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,37 +20,48 @@ export class HomeComponent implements OnInit {
   constructor(private mainServiceService: MainServiceService) {}
 
   ngOnInit() {
-  
   }
 
-  getCommand(){
+  getCommand() {
     this.isLoading = true;
     this.mainServiceService.getCommand()
     .finally(() => { this.isLoading = false; })
     .subscribe((command: any) => {
       this.command = command;
+      this.setmessageList(command);
       this.isLoading = true;
-      if(this.command.payload.command === 'request_link') {
+
+      if (this.command.payload.command === 'request_link') {
         this.mainServiceService.getLink()
         .finally(() => { this.isLoading = false; })
-        .subscribe((link: any) => { 
+        .subscribe((link: any) => {
           this.link = link
-          this.link.warnings.length !== 0 ? this.messageList.push(this.link.warnings) : false;
-          this.link.errors.length !== 0 ? this.messageList.push(this.link.errors) : false;
+          this.setmessageList(link);
+
         });
       }
 
-      if(this.command.payload.command === 'request_element') {
+      if (this.command.payload.command === 'request_element') {
         this.mainServiceService.getElement()
         .finally(() => { this.isLoading = false; })
-        .subscribe((element: any) => { 
+        .subscribe((element: any) => {
           this.element = element
-          this.element.warnings.length !== 0 ? this.messageList.push(this.element.warnings) : false;
-          this.element.errors.length !== 0 ? this.messageList.push(this.element.errors) : false;
+          this.setmessageList(element);
         });
       }
-      
      });
+  }
+
+  setmessageList(_item: any) {
+    if (_item.warnings.length !== 0) {
+      this.messageList.push(_item.warnings);
+    }
+    if (_item.errors.length !== 0) {
+      this.messageList.push(_item.warnings);
+    }
+    if (_item.payload.command) {
+      this.messageList.push([{code: 'HAPPY_REQUEST', details: ''}]);
+    }
   }
 
   clearData() {
